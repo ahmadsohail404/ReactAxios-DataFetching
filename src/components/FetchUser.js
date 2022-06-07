bae3d9648c12d0e91e8a28ff961620788f122e94
Sohail Ahmad <sohailss2412@gmail.com>
2022-06-07 14:30:08 +0530

    added hook callback

--- src/components/FetchUser.js
+++ src/components/FetchUser.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const FetchUser = () => {
     const [user, setUser] = useState([]);
+    const [clicked, isClicked] = useState(false)
 
     const url = "https://refertest.pythonanywhere.com/user/data"
 
@@ -22,26 +23,31 @@ const FetchUser = () => {
             <div className="container my-3" key={user.id}>
                 <div className="row">
                     <div className="col-3">
-                        <button className="btn btn-primary" onClick={fetchUser}>
+                        <button className="btn btn-primary" onClick={() => {
+                            fetchUser();
+                            isClicked(true);
+                        }}>
                             Fetch User Details &rarr;
                         </button>
                     </div>
                 </div>
             </div>
-
-            <div className="container my-3">
-                <div className="row">
-                    <div className="col-3 ">
-                        <div className="card" >
-                            <div className="card-body">
-                                <img src={user.pictureUrl} className="card-img-top my-2" alt="" />
-                                <h5 className="card-title">{user.name}</h5>
-                                <p className="card-text">{user.college}</p>
+            {
+                clicked &&
+                <div className="container my-3">
+                    <div className="row">
+                        <div className="col-3 ">
+                            <div className="card" >
+                                <div className="card-body">
+                                    <img src={user.pictureUrl} className="card-img-top my-2" alt="" />
+                                    <h5 className="card-title">{user.name}</h5>
+                                    <p className="card-text">{user.college}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            }
         </>
     );
 };
